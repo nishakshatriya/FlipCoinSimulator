63618dac8a1a98955261e8e62feb837bf1818d28
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-09 16:53:00 +0530

    Added: Stored Result in Dictonary

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -3,13 +3,37 @@
 #LOCAL VARIABLES
 isHead=1
 isTail=0
+HCount=0
+TCount=0
+
+declare -A dicResult
 
 echo "WELCOME TO FLIPCOIN SIMULATOR"
+read -p "how many times you want to Flip a coin?" trials
 
-result=$((RANDOM%2))
-if [ $result -eq $isHead ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+for((i=0;i<trials;i++))
+do
+
+	result=$((RANDOM%2))
+	if [ $result -eq $isHead ]
+	then
+		HCount=$((Hcount+1))
+		dicResult[heads]=$HCount
+	else
+		TCount=$((TCount+1))
+		dicResult[tail]=$TCount
+	fi
+	echo $result
+done
+
+total=$(( $HCount+$TCount ))
+head=$(( $HCount*100 ))
+headPercent=$(( $head/$total ))
+tail=$(( $TCount*100 ))
+tailPercent=$(( $tail/$total ))
+echo $headPercent
+echo $tailPercent
+
+
+
+
