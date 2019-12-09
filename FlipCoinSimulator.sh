84763320ed71784045256bf8daf243bc5b22e1a3
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-09 15:59:25 +0530

    Added: Calculated Head or Tail

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,2 +1,15 @@
 #!/bin/bash/ -x
+
+#LOCAL VARIABLES
+isHead=1
+isTail=0
+
 echo "WELCOME TO FLIPCOIN SIMULATOR"
+
+result=$((RANDOM%2))
+if [ $result -eq $isHead ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
